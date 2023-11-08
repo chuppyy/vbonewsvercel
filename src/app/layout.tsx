f4ef92bd8b6350e2300db1c969e3d9b12c881233
Thiện Tạ Khánh <takhanhthient3@gmail.com>
2023-11-08 16:12:54 +0700

    fg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FC51BKFSV2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FC51BKFSV2');
+        </script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
