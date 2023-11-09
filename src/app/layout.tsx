4c71f0a9c701d7f0ea3aa2907f994c5308ce87ae
Thiện Tạ Khánh <takhanhthient3@gmail.com>
2023-11-09 09:08:27 +0700

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from "next/script";
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +16,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">      
+    <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FC51BKFSV2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FC51BKFSV2');
+        </script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
